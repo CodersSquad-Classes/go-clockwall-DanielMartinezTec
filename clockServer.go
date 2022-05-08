0654d43472210e776fb51d617e01ecd73eb0c975
Daniel Martinez <A01631436@itesm.mx>
2022-05-08 17:32:47 -0500

    goserv

--- clockServer.go
+++ clockServer.go
@@ -1,17 +1,27 @@
-// Clock Server is a concurrent TCP server that periodically writes the time.
+// Clock2 is a concurrent TCP server that periodically writes the time.
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 	"time"
 )
 
 func handleConn(c net.Conn) {
 	defer c.Close()
+	loc, err1 := time.LoadLocation(os.Getenv("TZ"))
+	fmt.Println("user is connected to: " + os.Getenv("TZ"))
+
+	if err1 != nil {
+		fmt.Println("using local")
+		loc, err1 = time.LoadLocation("Local")
+	}
+	
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		_, err := io.WriteString(c, time.Now().In(loc).Format("15:04:05\n"))
 		if err != nil {
 			return // e.g., client disconnected
 		}
@@ -20,10 +30,15 @@ func handleConn(c net.Conn) {
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
+	if len(os.Args) < 3 || os.Args[1] != "-port" {
+		return
+	}
+
+	listener, err := net.Listen("tcp", "localhost:"+os.Args[2])
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
--- clockWall.go
+++ clockWall.go
@@ -1,5 +1,66 @@
 package main
 
-func main() {
-	// Let's start the fun
+import (
+	
+	"net"
+	"os"
+	"strings"
+	"fmt"
+	"io"
+	"log"
+
+)
+func mustCopy(zone string,src net.Conn,closer chan int) {
+
+   for new:=true;new;{
+	buf := make([]byte, 256)
+	n , err := src.Read(buf)
+
+        //_ , err := io.Copy(dst, src)
+
+
+        if err != nil {
+	    if err != io.EOF{
+                log.Fatal(err)
+		new=false
+
+	    } else {
+	        new=false
+	    }
+
+        } else {
+	    fmt.Print("["+zone+"] "+string(buf[:n]))
+	}
+    }
+
+
+    closer<-1
+}
+
+func main(){
+
+	chans:=make([]chan int,len(os.Args)-1)
+
+	if len(os.Args)>1 {
+
+		for i:=1;i<len(os.Args);i++ {
+			l:=strings.Split(os.Args[i],":")[1]
+			zone:=strings.Split(os.Args[i],"=")[0]
+			conn,err:=net.Dial("tcp","localhost:"+l)
+
+			if err != nil {
+     				log.Fatal(err)
+			}
+
+			chans[i-1]=make(chan int)
+			defer conn.Close()
+			go mustCopy(zone,conn,chans[i-1])
+		}
+	}
+	
+	for i:=0;i<len(chans);i++{
+		<-chans[i]
+		close(chans[i])
+	}
+
 }
